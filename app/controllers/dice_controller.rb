80749eb5e71186df90e2a17ab9efdd81dd36fe34
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-09-26 20:44:33 +0000

    dynamic dice is working

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,15 +1,30 @@
 class DiceController < ApplicationController
-  def twosix
+  def flex
+    @num_dice =  params.fetch(:number_of_dice).to_i
+
+    @sides = params.fetch("number_of_sides").to_i
+
     @rolls = []
-    2.times do 
-      die = rand(1..6)
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
       @rolls.push(die)
     end
-    render ({ :template => "templates/two_six"})
+    render ({ :template => "templates/flex"})
   end
-  
 
   def home 
     render ({ :template => "templates/home" })
   end
+
+  # not dynamic
+  # def twosix
+  #   @rolls = []
+  #   2.times do 
+  #     die = rand(1..6)
+  #     @rolls.push(die)
+  #   end
+  #   render ({ :template => "templates/two_six"})
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "dice", :action => "home"})
-  get("dice/2/6", { :controller => "dice", :action => "twosix"})
-  get("dice/2/10", { :controller => "dice", :action => "twoten"})
-  get("dice/1/20", { :controller => "dice", :action => "onetwenty"})
-  get("dice/5/4", { :controller => "dice", :action => "fivefour"})
+  get("dice/:number_of_dice/:number_of_sides", { :controller => "dice", :action => "flex"})
+  # not dynamic
+  # get("dice/2/6", { :controller => "dice", :action => "twosix"})
+  # get("dice/2/10", { :controller => "dice", :action => "two_ten"})
+  # get("dice/1/20", { :controller => "dice", :action => "onetwenty"})
+  # get("dice/5/4", { :controller => "dice", :action => "fivefour"})
 
 end
